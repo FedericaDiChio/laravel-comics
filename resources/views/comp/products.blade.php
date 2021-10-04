0a8a6e1f99ee2bb57cfddd69a73930be79ffa172
FedericaDiChio <federica.dichio7@gmail.com>
2021-10-04 08:34:06 +0200

    layout, header, main

---
+++ resources/views/comp/products.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main');
+
+@section('content')
+    <div class="container">
+        <h1>Current Series</h1>
+        <div class="container-card">
+            @foreach ($products as $product)
+                <div class="card">
+                    <figure>
+                        <img src="{{ $product['thumb'] }}" alt="">
+                    </figure>
+                    <address>{{ $product['series'] }}</address>
+                </div>  
+            @endforeach
+        </div>
+    </div>
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <nav></nav>
+    <div class="container">
+        <div class="row">
+            <ul>
+                <li><a href="{{ url('/characters') }}">Characters</a></li>
+                <li><a href="{{ url('/products') }}">Comics</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
